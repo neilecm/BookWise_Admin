8916b0cddc12a3f67ebbd9aa887b300f6f7f95e7
neilendrigo <neilendrigo@yahoo.com.br>
2025-12-01 00:52:38 +0700

    fix: Update CORS to bookwise.world without www

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/:path*",
+                source: "/api/public/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://www.bookwise.world" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Origin", value: "https://bookwise.world" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
